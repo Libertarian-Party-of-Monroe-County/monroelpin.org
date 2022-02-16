04b34dfd9f9fc64c1b0db4c7c84ddef13646f0c7
Matt Batman <batman.matt@gmail.com>
2022-02-16 18:55:20 -0500

    Install material ui, create a nav component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Nav from '../Nav/Nav';
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
       <main>{children}</main>
     </div>
   );
