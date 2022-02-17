4c151e8f835b73eae83e68c0ed2105456b9cb851
Matt Batman <batman.matt@gmail.com>
2022-02-16 19:34:18 -0500

    Add theming

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Link } from 'gatsby';
+import { Link } from 'gatsby-theme-material-ui';
 
 const Nav = () => {
   return (
