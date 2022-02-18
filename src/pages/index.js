bb11c27fbbd382e10e3e347e30f835a5ee787e77
Matt Batman <batman.matt@gmail.com>
2022-02-17 23:34:37 -0500

    Make home page responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,41 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { StaticImage } from 'gatsby-plugin-image';
 import { Link } from 'gatsby-theme-material-ui';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import * as homeStyles from './index.module.css';
 
 const IndexPage = () => {
+  const matches = useMediaQuery('(min-width:600px)');
+
   return (
     <Box className={homeStyles.Box} sx={{ backgroundColor: 'primary.main' }}>
       <div className={homeStyles.HomeLogo}>
-        <StaticImage
-          src="../images/torch.svg"
-          alt="liberty torch icon"
-          height={200}
-        />
+        {matches ? (
+          <StaticImage
+            src="../images/torch.svg"
+            alt="liberty torch icon"
+            height={200}
+          />
+        ) : (
+          <StaticImage
+            src="../images/torch.svg"
+            alt="liberty torch icon"
+            height={125}
+          />
+        )}
         <div className={homeStyles.HomeLogoTextBlock}>
-          <Typography component="div" variant="h2" color="white">
+          <Typography
+            component="div"
+            variant={matches ? 'h2' : 'h4'}
+            color="white"
+          >
             <b>Libertarian Party</b>
           </Typography>
-          <Typography component="div" variant="h2" color="white">
+          <Typography
+            component="div"
+            variant={matches ? 'h2' : 'h4'}
+            color="white"
+          >
             of <b>Monroe County</b>
           </Typography>
         </div>
