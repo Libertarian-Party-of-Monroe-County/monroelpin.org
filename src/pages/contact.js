7098b5b7af7624eb7cd3d7c40eec0e61d72987ef
Matt Batman <batman.matt@gmail.com>
2022-03-18 11:03:43 -0400

    Typography adjustments

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,56 +1,70 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import Layout from '../components/Layout/Layout';
 
 const ContactPage = () => (
   <Layout>
     <main>
       <title>Contact</title>
-      <h1>Contact</h1>
-      <h2>Leadership</h2>
-      <p>
-        <b>Chair:</b>{' '}
-        <Link href="mailto:chair@monroelpin.org">Dr. Damir Cavar</Link>
-      </p>
-      <p>
-        <b>Vice-Chair:</b> Matt Batman
-      </p>
-      <p>
-        <b>Treasurer:</b> Robert Milroy{' '}
-      </p>
-      <p>
-        <b>Secretary:</b> Dr. Joshua Herring
-      </p>
-      <h2>General:</h2>
-      <p>
-        <b>Info email:</b>{' '}
-        <Link href="mailto:info@monroelpin.org" color="primary">
-          info@monroelpin.org
-        </Link>
-      </p>
-      <p>
-        <b>Signal group:</b>{' '}
-        <Link href="https://signal.group/#CjQKICq65YYvulg0wXB_KOZeiVzJA2uowvdFkcAJsbHHUJzUEhDHfBFvnWa5LgeR5zGT4f8S">
-          Monroe County in Indiana Members
-        </Link>
-      </p>
-      <p>
-        <b>
-          <Link href="https://element.io/">Element</Link> (
-          <Link href="https://matrix.org/">Matrix</Link>):
-        </b>{' '}
-        <Link href="https://matrix.to/#/!aTMTZLDrrvBnFjiADl:matrix.org?via=matrix.org">
-          LPMC general room
-        </Link>
-      </p>
-      <p>
-        <b>
-          <Link href="https://ipfs.io/">IPFS</Link> links:
-        </b>{' '}
-        TBA
-      </p>
-      <h2>Libertarian Party of Indiana</h2>
+      <Typography component="h1" variant="h1" sx={{ mb: 4 }}>
+        Contact
+      </Typography>
+      <Typography component="h2" variant="h4">
+        Leadership
+      </Typography>
+      <List dense={true} sx={{ mb: 2 }}>
+        <ListItem disableGutters={true}>
+          <b>Chair:</b>&nbsp;
+          <Link href="mailto:chair@monroelpin.org">Dr. Damir Cavar</Link>
+        </ListItem>
+        <ListItem disableGutters={true}>
+          <b>Vice-Chair:</b>&nbsp;Matt Batman
+        </ListItem>
+        <ListItem disableGutters={true}>
+          <b>Treasurer:</b>&nbsp;Robert Milroy
+        </ListItem>
+        <ListItem disableGutters={true}>
+          <b>Secretary:</b>&nbsp;Dr. Joshua Herring
+        </ListItem>
+      </List>
+      <Typography component="h2" variant="h4">
+        General
+      </Typography>
+      <List dense={true} sx={{ mb: 2 }}>
+        <ListItem disableGutters={true}>
+          <b>Info email:</b>&nbsp;
+          <Link href="mailto:info@monroelpin.org" color="primary">
+            info@monroelpin.org
+          </Link>
+        </ListItem>
+        <ListItem disableGutters={true}>
+          <b>Signal group:</b>&nbsp;
+          <Link href="https://signal.group/#CjQKICq65YYvulg0wXB_KOZeiVzJA2uowvdFkcAJsbHHUJzUEhDHfBFvnWa5LgeR5zGT4f8S">
+            Monroe County in Indiana Members
+          </Link>
+        </ListItem>
+        <ListItem disableGutters={true}>
+          <b>
+            <Link href="https://element.io/">Element</Link> (
+            <Link href="https://matrix.org/">Matrix</Link>):
+          </b>&nbsp;
+          <Link href="https://matrix.to/#/!aTMTZLDrrvBnFjiADl:matrix.org?via=matrix.org">
+            LPMC general room
+          </Link>
+        </ListItem>
+        <ListItem disableGutters={true}>
+          <b>
+            <Link href="https://ipfs.io/">IPFS:</Link>
+          </b>&nbsp;
+          TBA
+        </ListItem>
+      </List>
+      <Typography component="h2" variant="h4">
+        Libertarian Party of Indiana
+      </Typography>
       <p>
         The Libertarian Party of Monroe County in Indiana (LPMC) is affiliated
         with the Libertarian Party of Indiana (LPIN).
@@ -65,10 +79,16 @@ const ContactPage = () => (
       </ul>
       <p></p>
       <p></p>
-      <h2>Libertarian Party</h2>
-      <p>
-        <Link href="https://www.lp.org/">Homepage</Link>
-      </p>
+      <Typography component="h2" variant="h4">
+        Libertarian Party
+      </Typography>
+      <ul>
+        <li>
+          <Link href="https://www.lp.org/">
+            National Libertarian Party Homepage
+          </Link>
+        </li>
+      </ul>
     </main>
   </Layout>
 );
